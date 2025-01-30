c4ee5a2862e3d9f88bcce851ae97e4deef4c2e6b
Shah Shalin <56588503+SH4LIN@users.noreply.github.com>
2025-01-30 22:13:24 +0530

    fix: CORS issue with the script module (#16)
    * fix: added a proxy API route which will make request to our WordPress server for the required module. Replaced the WordPress url from our module handle with our API route.
    
    * refactor: created a moduleAPIRoute variable which will have our API route details.
    
    * fix: replaced the url inside the importmap
    
    * fix: fixed the type issue with the GET API route
    
    * refactor: move cors proxy to be implemented as a middleware
    
    * refactor: rename cors middleware
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * refactor: handle src changes in the template renderer
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * refactor: rename cors middleware
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * docs: document cors middleware
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * chore: format
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * fix: unit test
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * fix: unit test
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * chore: cleanup
    
    * fix: add check if src url needs to be fixed
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * fix: rely on config flag
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * chore: revert + cleanup
    
    * doc: add doc for useCorsProxy flag
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * refactor: remove cors middleware export
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    Co-authored-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    Co-authored-by: Dovid Levine <david@axepress.dev>

---
+++ packages/next/src/middleware/cors.ts
@@ -0,0 +1,63 @@
+import { NextResponse } from 'next/server';
+import type { NextFetchEvent, NextMiddleware, NextRequest } from 'next/server';
+import { getConfig } from '@snapwp/core/config';
+import { MiddlewareFactory } from './utils';
+
+/**
+ * Facilitates proxying resources from WP resources. Any request with `corsProxyPrefix`
+ * as the first path element will be proxied to WP server.
+ *
+ * eg: http://localhost:3000/proxy/assets/api.js will get resouce at https://examplewp/assets/api.js
+ * assuming env vars NEXT_PUBLIC_URL had its value set to http://localhost:3000 and NEXT_PUBLIC_WORDPRESS_URL to https://examplewp.com
+ *
+ * @param  next - Next middleware
+ * @return The response object with modified headers
+ */
+export const corsProxyMiddleware: MiddlewareFactory = (
+	next: NextMiddleware
+) => {
+	return async ( request: NextRequest, _next: NextFetchEvent ) => {
+		const { homeUrl, corsProxyPrefix } = getConfig();
+
+		if ( ! request.nextUrl.pathname.startsWith( corsProxyPrefix ) ) {
+			return next( request, _next );
+		}
+
+		// Construct the target URL
+		const targetUrl =
+			homeUrl + request.nextUrl.pathname.replace( corsProxyPrefix, '' );
+		try {
+			// Forward the request to the external API
+			const response = await fetch( targetUrl, {
+				headers: {
+					'Content-Type': 'application/javascript', // Ensure the correct MIME type
+				},
+			} );
+
+			// Check if the response is OK
+			if ( ! response.ok ) {
+				throw new Error(
+					`Error from external API: ${ response.statusText }`
+				);
+			}
+
+			// Get the response data
+			const data = await response.text();
+
+			// Return the response with the correct Content-Type header
+			return new NextResponse( data, {
+				headers: {
+					'Content-Type': 'application/javascript',
+					'Content-Security-Policy': "default-src 'self'",
+				},
+			} );
+		} catch ( error ) {
+			// eslint-disable-next-line no-console
+			console.error( 'Proxy error:', error );
+			return NextResponse.json(
+				{ error: 'Internal Server Error' },
+				{ status: 500 }
+			);
+		}
+	};
+};
