834932f75acf9236aca1562068d84932dd356cc8
Swanand01 <swanandmathekar@gmail.com>
2025-02-07 18:55:24 +0530

    feat: add props type definitions for CoreCode

--- packages/blocks/src/blocks/core-code.tsx
+++ packages/blocks/src/blocks/core-code.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { getStylesFromAttributes } from '@snapwp/core';
+import { BlockData, getStylesFromAttributes } from '@snapwp/core';
 import { Parse } from '@snapwp/next';
+
+interface CoreCodeAttributes {
+	style?: string;
+	content?: string;
+	cssClassName?: string;
+}
+
+export interface CoreCodeProps extends BlockData {
+	attributes?: CoreCodeAttributes;
+}
 
 /**
  * Renders the core/code block.
@@ -10,7 +20,7 @@ import { Parse } from '@snapwp/next';
  *
  * @return The rendered block.
  */
-export default function CoreCode( { attributes }: any ) {
+export default function CoreCode( { attributes }: CoreCodeProps ) {
 	const { style, cssClassName, content } = attributes || {};
 	const styleObject = getStylesFromAttributes( { style } );
 
