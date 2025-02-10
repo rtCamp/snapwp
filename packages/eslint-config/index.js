85bd9a09d6becca015b5bb3b7321e68840ca1fc2
Amaan Khan <amaanpathan5@gmail.com>
2025-02-10 19:12:37 +0530

    refactor: update eslint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -5,8 +5,9 @@ module.exports = {
 		es6: true,
 	},
 	parser: '@typescript-eslint/parser',
-	plugins: [ 'jsdoc', 'import', 'n' ],
+	plugins: [ '@typescript-eslint', 'jsdoc', 'jest', 'import', 'n' ],
 	extends: [
+		'eslint:recommended',
 		'plugin:jsdoc/recommended-typescript',
 		'plugin:import/typescript',
 	],
@@ -57,6 +58,16 @@ module.exports = {
 				'import/default': [ 'off' ],
 			},
 		},
+		{
+			files: [
+				'bin/**/*.js',
+				'bin/**/*.mjs',
+				'packages/cli/src/*.cjs',
+			],
+			rules: {
+				'no-console': 'off',
+			},
+		},
 		// Disable n/no-process-env for codegen.ts file.
 		{
 			files: [ '**/codegen.ts', '**/*.test.*', '**/jest.setup.js' ],
@@ -64,10 +75,92 @@ module.exports = {
 				'n/no-process-env': 'off',
 			},
 		},
+		{
+			files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+			excludedFiles: [
+				'**/*.test.ts',
+				'**/*.test.tsx',
+				'**/*.spec.ts',
+				'**/*.spec.tsx',
+				'**/codegen.ts',
+			],
+			extends: [
+				'plugin:@typescript-eslint/recommended-type-checked'
+			],
+			parserOptions: {
+				project: true,
+			},
+			rules: {
+				'@typescript-eslint/no-array-delete': 'error',
+			},
+		},
+		{
+			files: ['**/jest.*.js', '**/*.cjs', '**/*.test.ts'],
+			env: {
+				node: true,
+				'jest/globals': true,
+			},
+			rules: {
+				'no-empty-function': 'off',
+				'@typescript-eslint/no-empty-function': 'off',
+				'eslint-comments/require-description': 'off',
+				'@typescript-eslint/no-require-imports': 'off',
+			},
+		},
+		{
+			files: ["**/codegen.ts", "**/*.test.*"],
+			rules: {
+				"n/no-process-env": "off"
+			},
+		},
 	],
 	rules: {
 		'n/no-process-env': [ 'error' ],
 		'jsdoc/no-types': [ 'off' ],
-		'jsdoc/require-param-type': [ 'warn' ], // @todo: Convert current rule to error after @param types are added.
+		'jsdoc/require-param-type': [ 'error' ],
+		'jsdoc/require-returns-type': [ 'error' ],
+		'dot-notation': [ "error", { allowKeywords: false } ],
+		'@typescript-eslint/naming-convention': 'error',
+		'jest/expect-expect': 'off',
+		'react/jsx-boolean-value': 'error',
+		'@wordpress/dependency-group': 'error',
+		'import/default': 'error',
+		'import/named': 'error',
+		'no-empty-function': 'error',
+		'no-restricted-syntax': [ 'error' ],
+		'jsx-closing-tag-location': [ 1, { location: 'line-aligned' } ],
+		'eslint-comments/require-description': 'error',
+		'react/jsx-curly-brace-presence': [
+			'error',
+			{ props: 'never', children: 'never' },
+		],
+		'prefer-destructuring': [
+			'error',
+			{
+				array: true,
+				object: true,
+			},
+		],
+		'no-restricted-imports': [
+			'error',
+			{
+				paths: [
+					{
+						name: 'lodash',
+						message: 'Please use native functionality instead.',
+					},
+					{
+						name: 'classnames',
+						message:
+							'Please use `clsx` instead. It\'s a lighter and faster drop-in replacement for `classnames`.',
+					},
+				],
+			},
+		],
+		'@typescript-eslint/no-restricted-imports': [
+			'error',
+			'classnames',
+			'lodash',
+		],
 	},
 };
