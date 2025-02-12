59e7cd00a939fe1ea0e5c5fd44f4319e0088538e
Swanand01 <swanandmathekar@gmail.com>
2025-02-12 11:45:06 +0530

    feat: update BlockDefinitions type

--- packages/blocks/src/blocks/index.ts
+++ packages/blocks/src/blocks/index.ts
@@ -1,38 +1,81 @@
 import { BlockData } from '..';
-import CoreAudio from './core-audio';
-import CoreButton from './core-button';
-import CoreButtons from './core-buttons';
-import CoreCode from './core-code';
-import CoreColumn from './core-column';
-import CoreColumns from './core-columns';
-import CoreCover from './core-cover';
-import CoreDetails from './core-details';
-import CoreFile from './core-file';
+import CoreAudio, { CoreAudioAttributes } from './core-audio';
+import CoreButton, { CoreButtonAttributes } from './core-button';
+import CoreButtons, { CoreButtonsAttributes } from './core-buttons';
+import CoreCode, { CoreCodeAttributes } from './core-code';
+import CoreColumn, { CoreColumnAttributes } from './core-column';
+import CoreColumns, { CoreColumnsAttributes } from './core-columns';
+import CoreCover, { CoreCoverAttributes } from './core-cover';
+import CoreDetails, { CoreDetailsAttributes } from './core-details';
+import CoreFile, { CoreFileAttributes } from './core-file';
 import CoreFreeform from './core-freeform';
-import CoreGallery from './core-gallery';
-import CoreGroup from './core-group';
-import CoreHeading from './core-heading';
+import CoreGallery, { CoreGalleryAttributes } from './core-gallery';
+import CoreGroup, { CoreGroupAttributes } from './core-group';
+import CoreHeading, { CoreHeadingAttributes } from './core-heading';
 import CoreHtml from './core-html';
-import CoreImage from './core-image';
-import CoreList from './core-list';
-import CoreListItem from './core-list-item';
-import CoreMediaText from './core-media-text';
-import CoreParagraph from './core-paragraph';
+import CoreImage, { CoreImageAttributes } from './core-image';
+import CoreList, { CoreListAttributes } from './core-list';
+import CoreListItem, { CoreListItemAttributes } from './core-list-item';
+import CoreMediaText, { CoreMediaTextAttributes } from './core-media-text';
+import CoreParagraph, { CoreParagraphAttributes } from './core-paragraph';
 import CorePattern from './core-pattern';
 import CorePostContent from './core-post-content';
-import CorePreformatted from './core-preformatted';
-import CorePullquote from './core-pullquote';
-import CoreQuote from './core-quote';
+import CorePreformatted, {
+	CorePreformattedAttributes,
+} from './core-preformatted';
+import CorePullquote, { CorePullquoteAttributes } from './core-pullquote';
+import CoreQuote, { CoreQuoteAttributes } from './core-quote';
 import CoreSeparator from './core-separator';
-import CoreSpacer from './core-spacer';
-import CoreTemplatePart from './core-template-part';
-import CoreVerse from './core-verse';
-import CoreVideo from './core-video';
+import CoreSpacer, { CoreSpacerAttributes } from './core-spacer';
+import CoreTemplatePart, {
+	CoreTemplatePartAttributes,
+} from './core-template-part';
+import CoreVerse, { CoreVerseAttributes } from './core-verse';
+import CoreVideo, { CoreVideoAttributes } from './core-video';
 import Default from './default';
 
-export type BlockDefinitions = {
-	[ key: string ]: React.FC< BlockData >;
-};
+export interface BlockTypeMap {
+	CoreAudio: CoreAudioAttributes;
+	CoreButton: CoreButtonAttributes;
+	CoreButtons: CoreButtonsAttributes;
+	CoreCode: CoreCodeAttributes;
+	CoreColumn: CoreColumnAttributes;
+	CoreColumns: CoreColumnsAttributes;
+	CoreCover: CoreCoverAttributes;
+	CoreDetails: CoreDetailsAttributes;
+	CoreFile: CoreFileAttributes;
+	CoreFreeform: {};
+	CoreGallery: CoreGalleryAttributes;
+	CoreGroup: CoreGroupAttributes;
+	CoreHeading: CoreHeadingAttributes;
+	CoreHtml: {};
+	CoreImage: CoreImageAttributes;
+	CoreList: CoreListAttributes;
+	CoreListItem: CoreListItemAttributes;
+	CoreMediaText: CoreMediaTextAttributes;
+	CorePattern: {};
+	CoreParagraph: CoreParagraphAttributes;
+	CorePostContent: {};
+	CorePreformatted: CorePreformattedAttributes;
+	CorePullquote: CorePullquoteAttributes;
+	CoreQuote: CoreQuoteAttributes;
+	CoreSeparator: {};
+	CoreSpacer: CoreSpacerAttributes;
+	CoreTemplatePart: CoreTemplatePartAttributes;
+	CoreVerse: CoreVerseAttributes;
+	CoreVideo: CoreVideoAttributes;
+	default: {};
+}
+
+export type BlockDefinitions = Partial<
+	{
+		[ K in keyof BlockTypeMap ]: React.FC<
+			BlockData< BlockTypeMap[ K ] >
+		> | null;
+	} & {
+		[ key: string ]: React.FC< BlockData< any > > | null;
+	}
+>;
 
 const blocks: BlockDefinitions = {
 	CoreAudio,
