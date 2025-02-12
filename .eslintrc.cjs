97040b8fd27c91800435de91bdbdab142ac8e9f4
Amaan Khan <amaanpathan5@gmail.com>
2025-02-12 23:04:04 +0530

    refactor: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
 		node: true,
 	},
 	extends: '@snapwp/eslint-config',
+	parser: '@typescript-eslint/parser',
 	ignorePatterns: [
 		'**/node_modules/**',
 		'**/dist/**',
@@ -15,6 +16,8 @@ module.exports = {
 		'**/config/*.js',
 		'**/dist',
 		'**/__generated/',
+		'**/.eslintrc.cjs',
+		'**/eslint-config/**',
 	],
 	globals: {
 		globalThis: 'readonly',
@@ -23,11 +26,70 @@ module.exports = {
 		'import/resolver': require.resolve( './config/import-resolver.cjs' ),
 	},
 	overrides: [
-		// Disable n/no-process-env for codegen.ts file
+		{
+			files: '**/*.test.ts',
+			env: {
+				jest: true,
+			},
+		},
+		{
+			files: [ '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx' ],
+			rules: {
+				'jsdoc/require-jsdoc': [
+					'error',
+					{
+						require: {
+							ArrowFunctionExpression: true,
+							ClassDeclaration: true,
+							ClassExpression: true,
+							FunctionExpression: true,
+							MethodDefinition: true,
+						},
+					},
+				],
+				'import/default': [ 'off' ],
+			},
+		},
+		{
+			files: [ 'bin/**/*.js', 'bin/**/*.mjs', 'packages/cli/src/*.cjs' ],
+			rules: {
+				'no-console': 'off',
+			},
+		},
 		{
 			files: [ '**/codegen.ts', '**/*.test.*', '**/jest.setup.js' ],
 			rules: {
 				'n/no-process-env': 'off',
+			},
+		},
+		{
+			files: [ '**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts' ],
+			excludedFiles: [
+				'**/*.test.ts',
+				'**/*.test.tsx',
+				'**/*.spec.ts',
+				'**/*.spec.tsx',
+				'**/codegen.ts',
+			],
+			extends: [ 'plugin:@typescript-eslint/recommended-type-checked' ],
+			parserOptions: {
+				project: true,
+			},
+			rules: {
+				'@typescript-eslint/no-array-delete': 'error',
+			},
+		},
+		{
+			files: [ '**/jest.*.js', '**/*.cjs', '**/*.test.ts' ],
+			env: {
+				node: true,
+				'jest/globals': true,
+			},
+			rules: {
+				'no-empty-function': 'off',
+				'@typescript-eslint/no-empty-function': 'off',
+				'eslint-comments/require-description': 'off',
+				'@typescript-eslint/no-require-imports': 'off',
 			},
 		},
 	],
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,42 +4,67 @@ module.exports = {
 		browser: true,
 		es6: true,
 	},
-	parser: '@typescript-eslint/parser',
-	plugins: [ '@wordpress/eslint-plugin', 'jsdoc', 'import', 'n' ],
+	plugins: [
+		'@wordpress/eslint-plugin',
+		'@typescript-eslint',
+		'jsdoc',
+		'jest',
+		'import',
+		'n',
+	],
 	extends: [
 		'plugin:@wordpress/eslint-plugin/recommended',
 		'plugin:jsdoc/recommended-typescript',
 		'plugin:import/typescript',
-	],
-	ignorePatterns: [ '**/config/*.js', '**/dist' ],
-	settings: {},
-	overrides: [
-		{
-			files: '**/*.test.ts',
-			env: {
-				jest: true,
-			},
-		},
-		{
-			files: [ '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx' ],
-			rules: {
-				'jsdoc/require-jsdoc': [
-					'error',
-					{
-						require: {
-							ArrowFunctionExpression: true,
-							ClassDeclaration: true,
-							ClassExpression: true,
-							FunctionExpression: true,
-							MethodDefinition: true,
-						},
-					},
-				],
-				'import/default': [ 'off' ],
-			},
-		},
+		'eslint:recommended',
 	],
 	rules: {
 		'n/no-process-env': [ 'error' ],
+		'jsdoc/no-types': [ 'off' ],
+		'jsdoc/require-param-type': [ 'error' ],
+		'jsdoc/require-returns-type': [ 'error' ],
+		'dot-notation': [ 'error', { allowKeywords: false } ],
+		'@typescript-eslint/naming-convention': 'error',
+		'jest/expect-expect': 'off',
+		'react/jsx-boolean-value': 'error',
+		'@wordpress/dependency-group': 'error',
+		'import/default': 'error',
+		'import/named': 'error',
+		'no-empty-function': 'error',
+		'no-restricted-syntax': [ 'error' ],
+		'jsx-closing-tag-location': [ 1, { location: 'line-aligned' } ],
+		'eslint-comments/require-description': 'error',
+		'react/jsx-curly-brace-presence': [
+			'error',
+			{ props: 'never', children: 'never' },
+		],
+		'prefer-destructuring': [
+			'error',
+			{
+				array: true,
+				object: true,
+			},
+		],
+		'no-restricted-imports': [
+			'error',
+			{
+				paths: [
+					{
+						name: 'lodash',
+						message: 'Please use native functionality instead.',
+					},
+					{
+						name: 'classnames',
+						message:
+							"Please use `clsx` instead. It's a lighter and faster drop-in replacement for `classnames`.",
+					},
+				],
+			},
+		],
+		'@typescript-eslint/no-restricted-imports': [
+			'error',
+			'classnames',
+			'lodash',
+		],
 	},
 };
