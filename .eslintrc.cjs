7234ac8519cb5798f9484070d48bb9f8a19fafed
Amaan Khan <amaanpathan5@gmail.com>
2025-02-20 16:23:10 +0530

    refactor: reformat base eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,20 @@ module.exports = {
 		node: true,
 	},
 	extends: '@snapwp/eslint-config',
+	parser: '@typescript-eslint/parser',
 	ignorePatterns: [
-		'**/node_modules/**',
-		'**/dist/**',
-		'**/dist-types/**',
-		'out/**',
-		'data/**',
-		'assets/**/*.js',
-		'coverage/**',
+		'**/__generated/',
+		'**/.eslintrc.cjs',
 		'**/config/*.js',
 		'**/dist',
-		'**/__generated/',
+		'**/dist/**',
+		'**/dist-types/**',
+		'**/eslint-config/**',
+		'**/node_modules/**',
+		'assets/**/*.js',
+		'data/**',
+		'coverage/**',
+		'out/**',
 	],
 	globals: {
 		globalThis: 'readonly',
@@ -22,7 +25,55 @@ module.exports = {
 	settings: {
 		'import/resolver': require.resolve( './config/import-resolver.cjs' ),
 	},
+	rules: {
+		'@typescript-eslint/no-restricted-imports': [
+			'error',
+			'classnames',
+			'lodash',
+		],
+		'import/default': 'error',
+		'import/named': 'error',
+		'jest/expect-expect': 'off',
+		'jsdoc/no-types': [ 'off' ],
+		'no-restricted-imports': [
+			'error',
+			{
+				paths: [
+					{
+						name: 'classnames',
+						message:
+							"Please use `clsx` instead. It's a lighter and faster drop-in replacement for `classnames`.",
+					},
+					{
+						name: 'lodash',
+						message: 'Please use native functionality instead.',
+					},
+				],
+			},
+		],
+		'no-restricted-syntax': [ 'error' ],
+	},
 	overrides: [
+		// Rules for JS, JSX, TS & TSX files.
+		{
+			files: [ '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx' ],
+			// Mandate doc block for arrow functions, class declarations, class expressions, function expressions, and method definition.
+			rules: {
+				'jsdoc/require-jsdoc': [
+					'error',
+					{
+						require: {
+							ArrowFunctionExpression: true,
+							ClassDeclaration: true,
+							ClassExpression: true,
+							FunctionExpression: true,
+							MethodDefinition: true,
+						},
+					},
+				],
+				'import/default': [ 'off' ],
+			},
+		},
 		// Disable n/no-process-env for codegen.ts file
 		{
 			files: [ '**/codegen.ts', '**/*.test.*', '**/jest.setup.js' ],
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -5,39 +5,19 @@ module.exports = {
 		es6: true,
 	},
 	parser: '@typescript-eslint/parser',
-	plugins: [ '@wordpress/eslint-plugin', 'jsdoc', 'import', 'n' ],
+	plugins: [
+		'@typescript-eslint',
+		'@wordpress/eslint-plugin',
+		'import',
+		'n',
+		'jest',
+		'jsdoc',
+		'@stylistic/jsx',
+	],
 	extends: [
 		'plugin:@wordpress/eslint-plugin/recommended',
 		'plugin:jsdoc/recommended-typescript',
 		'plugin:import/typescript',
-	],
-	ignorePatterns: [ '**/config/*.js', '**/dist' ],
-	settings: {},
-	overrides: [
-		{
-			files: '**/*.test.ts',
-			env: {
-				jest: true,
-			},
-		},
-		{
-			files: [ '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx' ],
-			rules: {
-				'jsdoc/require-jsdoc': [
-					'error',
-					{
-						require: {
-							ArrowFunctionExpression: true,
-							ClassDeclaration: true,
-							ClassExpression: true,
-							FunctionExpression: true,
-							MethodDefinition: true,
-						},
-					},
-				],
-				'import/default': [ 'off' ],
-			},
-		},
 	],
 	rules: {
 		'n/no-process-env': [ 'error' ],
