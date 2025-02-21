1138bbc62b72cb028d53d7686523708a99e676e6
Swanand01 <swanandmathekar@gmail.com>
2025-02-21 19:19:05 +0530

    refactor: re-add `blockDefinitions` to `EditorBlocksRendererProps`

--- packages/blocks/src/editor-blocks-renderer/index.tsx
+++ packages/blocks/src/editor-blocks-renderer/index.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 import BlockManager from '@/block-manager';
 import { getConfig } from '@snapwp/core/config';
 
-import type { BlockData, BlockTreeNode } from '@snapwp/types';
+import type { BlockData, BlockDefinitions, BlockTreeNode } from '@snapwp/types';
 
 type EditorBlocksRendererProps = {
 	editorBlocks?: BlockData[] | null;
+	blockDefinitions?: BlockDefinitions | null;
 };
 
 /**
  * A react component to render editor blocks.
  * @param props - Props.
  * @param props.editorBlocks - A list of blocks to be rendered.
+ * @param props.blockDefinitions - Blocks rendering functions.
  * @return The rendered template
  */
 export default function EditorBlocksRenderer( {
 	editorBlocks,
+	blockDefinitions,
 }: EditorBlocksRendererProps ) {
-	const { blockDefinitions } = getConfig();
+	const { blockDefinitions: globalBlockDefinitions } = getConfig();
 
-	if ( blockDefinitions ) {
-		BlockManager.addBlockDefinitions( blockDefinitions );
+	const resolvedBlockDefinitions = globalBlockDefinitions ?? blockDefinitions;
+
+	if ( resolvedBlockDefinitions ) {
+		BlockManager.addBlockDefinitions( resolvedBlockDefinitions );
 	}
 
 	const parsedTree = BlockManager.parseBlockForRendering( editorBlocks );
