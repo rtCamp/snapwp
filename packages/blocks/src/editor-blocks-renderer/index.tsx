703f7ff5e908cef7bf0a3c194fe12ef2ad1e1d69
Swanand01 <swanandmathekar@gmail.com>
2025-02-21 14:29:00 +0530

    format: add whitespace

--- packages/blocks/src/editor-blocks-renderer/index.tsx
+++ packages/blocks/src/editor-blocks-renderer/index.tsx
@@ -19,6 +19,7 @@ export default function EditorBlocksRenderer( {
 	editorBlocks,
 }: EditorBlocksRendererProps ) {
 	const { blockDefinitions } = getConfig();
+
 	if ( blockDefinitions ) {
 		BlockManager.addBlockDefinitions( blockDefinitions );
 	}
