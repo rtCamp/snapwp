f4e49bc00f9611bc85a373ae890ad6dc1827c8f5
Amaan Khan <amaanpathan5@gmail.com>
2025-02-25 03:18:05 +0530

    feat: add recommended rulesets for eslint comments

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -9,6 +9,7 @@ module.exports = {
 		'plugin:@wordpress/eslint-plugin/recommended',
 		'plugin:jsdoc/recommended-typescript',
 		'plugin:import/typescript',
+		'plugin:@eslint-community/eslint-comments/recommended',
 	],
 	rules: {
 		'n/no-process-env': [ 'error' ],
