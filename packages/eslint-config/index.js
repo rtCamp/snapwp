50be1d37564d0c7c742e08400a059ab47ad5a446
Dovid Levine <david@axepress.dev>
2025-02-25 12:21:53 +0200

    chore: relocate to repo ruleset

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -6,7 +6,6 @@ module.exports = {
 	},
 	plugins: [ '@wordpress/eslint-plugin', 'import', 'jsdoc', 'n' ],
 	extends: [
-		'plugin:@eslint-community/eslint-comments/recommended',
 		'plugin:@wordpress/eslint-plugin/recommended',
 		'plugin:import/typescript',
 		'plugin:jsdoc/recommended-typescript',
