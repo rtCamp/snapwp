764aa598ceda538e56dc1dc1bb74458c337c634e
Swanand01 <swanandmathekar@gmail.com>
2025-02-27 17:41:39 +0530

    refactor: ignore `n/no-process-env` and `no-console` for e2e-tests/utils

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -116,5 +116,12 @@ module.exports = {
 				project: true,
 			},
 		},
+		{
+			files: [ '**/packages/e2e-tests/src/utils/*.ts' ],
+			rules: {
+				'n/no-process-env': 'off',
+				'no-console': 'off',
+			},
+		},
 	],
 };
