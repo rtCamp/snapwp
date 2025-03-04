c5210851ad301477357790802f5e67bd043b36dd
Swanand01 <swanandmathekar@gmail.com>
2025-03-04 15:45:05 +0530

    refactor: add setup

---
+++ packages/e2e-tests/src/utils/setup.ts
@@ -0,0 +1,17 @@
+import { test as setup } from '@playwright/test';
+import setupWordPress from './setup-wordpress';
+import setupNextApp from './setup-next-app';
+import startNextApp from './start-next-app';
+
+setup( 'setup WordPress and the Next.js app', async () => {
+	setup.setTimeout( 600000 );
+	try {
+		await setupWordPress();
+		await setupNextApp();
+		await startNextApp();
+		console.log( 'Environment setup complete!' );
+	} catch ( error ) {
+		console.error( 'Environment setup failed:', error );
+		process.exit( 1 );
+	}
+} );
