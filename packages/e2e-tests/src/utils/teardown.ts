813a1a42416e76c0b429f0066c80f40960d3ca26
Swanand01 <swanandmathekar@gmail.com>
2025-03-05 19:44:04 +0530

    refactor: use `cleanup` in `teardown`

--- packages/e2e-tests/src/utils/teardown.ts
+++ packages/e2e-tests/src/utils/teardown.ts
@@ -1,19 +1,5 @@
-import { execSync } from 'child_process';
 import { test as teardown } from '@playwright/test';
+import cleanup from './cleanup';
 
-teardown( 'shut down WordPress and the Next.js app', () => {
-	teardown.setTimeout( 600000 );
-	try {
-		if ( process.env[ 'NEXT_PID' ] ) {
-			console.log( 'Stopping Next.js...' );
-			process.kill( parseInt( process.env[ 'NEXT_PID' ] ) );
-		}
-
-		console.log( 'Stopping WordPress...' );
-		execSync( 'npm run wp-env stop', { stdio: 'inherit' } );
-
-		console.log( 'Cleanup completed.' );
-	} catch ( error ) {
-		console.error( 'Error during global teardown:', error );
-	}
-} );
+teardown.describe.configure( { timeout: 600000 } );
+teardown( 'shut down WordPress and the Next.js app', cleanup );
