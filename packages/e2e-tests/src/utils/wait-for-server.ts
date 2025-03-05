94fbf31ab2d04e1f1e80d5fd836476768e6ad400
Swanand01 <swanandmathekar@gmail.com>
2025-03-05 20:18:07 +0530

    refactor: `waitForServer`

--- packages/e2e-tests/src/utils/wait-for-server.ts
+++ packages/e2e-tests/src/utils/wait-for-server.ts
@@ -4,7 +4,6 @@
  * @param url - The server URL to check.
  * @param timeout - Max wait time per attempt in ms.
  * @param retries - Number of retry attempts (default: 5).
- * @return Resolves `true` if the server responds.
  * @throws If the server doesn't respond after all retries.
  */
 export default async function waitForServer(
@@ -12,32 +11,18 @@ export default async function waitForServer(
 	timeout = 10000,
 	retries = 5
 ) {
-	let attempts = 0;
-
-	return new Promise< boolean >( ( resolve, reject ) => {
-		const interval = setInterval( async () => {
-			attempts++;
-
-			try {
-				const response = await fetch( url, {
-					signal: AbortSignal.timeout( timeout ),
-				} );
-				if ( response.ok ) {
-					clearInterval( interval );
-					return resolve( true );
-				}
-			} catch ( error ) {
-				// Ignore errors and retry
-			}
-
-			if ( attempts >= retries ) {
-				clearInterval( interval );
-				return reject(
-					new Error(
-						`Server at ${ url } did not respond after ${ retries } attempts.`
-					)
-				);
-			}
-		}, 1000 );
-	} );
+	try {
+		await fetch( url, {
+			method: 'HEAD',
+			signal: AbortSignal.timeout( timeout ),
+		} );
+	} catch ( error ) {
+		if ( retries > 0 ) {
+			console.log( 'Retrying...' );
+			await new Promise( ( resolve ) => setTimeout( resolve, 1000 ) );
+			await waitForServer( url, timeout, retries - 1 );
+		} else {
+			throw error;
+		}
+	}
 }
