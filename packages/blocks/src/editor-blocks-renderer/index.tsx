60e5dbdf04282386e341ad66c35bc473b6e98dc7
Amaan Khan <amaanpathan5@gmail.com>
2025-03-11 22:28:36 +0530

    refactor: add param type

--- packages/blocks/src/editor-blocks-renderer/index.tsx
+++ packages/blocks/src/editor-blocks-renderer/index.tsx
@@ -11,9 +11,9 @@ type EditorBlocksRendererProps = {
 /**
  * A react component to render editor blocks.
  *
- * @param {Object}                                 props                  Props.
- * @param {EditorBlocksRendererProps.editorBlocks} props.editorBlocks     A list of blocks to be rendered.
- * @param {EditorBlocksRendererProps}              props.blockDefinitions Blocks rendering functions.
+ * @param {Object}                                     props                  Props.
+ * @param {EditorBlocksRendererProps.editorBlocks}     props.editorBlocks     A list of blocks to be rendered.
+ * @param {EditorBlocksRendererProps.blockDefinitions} props.blockDefinitions Blocks rendering functions.
  *
  * @return The rendered template
  */
