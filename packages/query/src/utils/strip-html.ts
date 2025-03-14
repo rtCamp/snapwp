59a63492e86246be307bcde97270bc30fc741462
Swanand01 <swanandmathekar@gmail.com>
2025-03-14 18:27:04 +0530

    chore: remove unused files

--- packages/query/src/utils/strip-html.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Removes all HTML tags from a given string.
- *
- * @param html - The HTML string to be stripped.
- * @return The cleaned text without HTML tags.
- */
-export default function stripHtml( html: string ) {
-	return html.replace( /<\/?[^>]+(>|$)/g, '' ).trim();
-}
