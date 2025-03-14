48e2077c97b6ea9ca9478809b8c6fa06149ddab3
Swanand01 <swanandmathekar@gmail.com>
2025-03-14 18:30:54 +0530

    feat: use `generateMetadata` in layout and page

--- examples/nextjs/starter/src/app/[[...path]]/page.tsx
+++ examples/nextjs/starter/src/app/[[...path]]/page.tsx
@@ -1,5 +1,49 @@
 import { TemplateRenderer } from '@snapwp/next';
 import { EditorBlocksRenderer } from '@snapwp/blocks';
+import { QueryEngine } from '@snapwp/query';
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
+import { getConfig } from '@snapwp/core/config';
+
+export async function generateMetadata(): Promise< Metadata | undefined > {
+	const headerList = await headers();
+	const uri = headerList.get( 'x-current-path' ) || '/';
+
+	if ( uri === '/' ) {
+		return undefined;
+	}
+
+	const { nextUrl } = getConfig();
+
+	const routeMetadata = await QueryEngine.getRouteMetadata( uri );
+	const openGraphMetadata = await QueryEngine.getOpenGraphMetadata( uri );
+	const twitterMetadata = await QueryEngine.getTwitterMetadata( uri );
+
+	return {
+		title: routeMetadata.title,
+		description: routeMetadata.description,
+		authors: routeMetadata.authors,
+		openGraph: {
+			title: openGraphMetadata.title ?? routeMetadata.title,
+			url: `${ nextUrl }${ uri }`,
+			...( openGraphMetadata.type && { type: openGraphMetadata.type } ),
+			images: openGraphMetadata.images,
+			publishedTime: openGraphMetadata.publishedTime,
+			modifiedTime: openGraphMetadata.modifiedTime,
+			description:
+				openGraphMetadata.description ?? routeMetadata.description,
+		},
+		alternates: {
+			canonical: `${ nextUrl }${ uri }`,
+		},
+		twitter: {
+			title: twitterMetadata.title ?? routeMetadata.title,
+			description:
+				twitterMetadata.description ?? routeMetadata.description,
+			images: twitterMetadata.image ? [ twitterMetadata.image ] : [],
+		},
+	};
+}
 
 export default function Page() {
 	return (
--- examples/nextjs/starter/src/app/layout.tsx
+++ examples/nextjs/starter/src/app/layout.tsx
@@ -1,4 +1,29 @@
+import { getConfig } from '@snapwp/core/config';
 import { RootLayout } from '@snapwp/next';
+import { QueryEngine } from '@snapwp/query';
+import { Metadata } from 'next';
+
+export async function generateMetadata(): Promise< Metadata > {
+	const globalMetadata = await QueryEngine.getGlobalMetadata();
+	const { nextUrl } = getConfig();
+
+	return {
+		title: globalMetadata.siteTitle,
+		applicationName: globalMetadata.siteTitle,
+		description: globalMetadata.description,
+		openGraph: {
+			title: globalMetadata.siteTitle,
+			description: globalMetadata.description,
+			locale: globalMetadata.locale,
+			siteName: globalMetadata.siteTitle,
+			url: nextUrl,
+		},
+		twitter: {
+			title: globalMetadata.siteTitle,
+			description: globalMetadata.description,
+		},
+	};
+}
 
 export default function Layout( { children }: { children: React.ReactNode } ) {
 	return (
