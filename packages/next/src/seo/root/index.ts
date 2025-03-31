aab34bba35ea29254137618c8e6bada20395c37d
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2025-03-31 12:14:06 +0530

    fix: add better error handelling
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- packages/next/src/seo/root/index.ts
+++ packages/next/src/seo/root/index.ts
@@ -14,8 +14,11 @@ import parseSiteMetadata from './site-metadata/parser';
 interface RootMetadataGeneratorPlugin< TData > {
 	name: string;
 	fetcher: Fetcher;
+	defaultFetchedObject?: unknown;
 	validator: Validator< TData >;
+	defaultValidatedObject?: TData;
 	parser: Parser< TData >;
+	defaultParsedObject?: Metadata;
 }
 
 const plugins: Record< string, RootMetadataGeneratorPlugin< unknown > > = {};
@@ -29,7 +32,6 @@ export function registerMetadataGenerator< T extends unknown >(
 	generator: RootMetadataGeneratorPlugin< T >,
 	key: string
 ): void {
-	// @todo How should plugins with the same key handeled
 	plugins[ key ] = generator as RootMetadataGeneratorPlugin< unknown >;
 }
 
@@ -37,6 +39,11 @@ const IconsMetaDataGenerator: RootMetadataGeneratorPlugin< IconsMetaData > = {
 	name: 'icon',
 	fetcher: fetchIcons,
 	validator: validateIconMetadata,
+	defaultValidatedObject: {
+		faviconIcons: [],
+		appleIcons: undefined,
+		msApplicationTileIcon: undefined,
+	},
 	parser: parseIconMetadata,
 };
 
@@ -47,16 +54,12 @@ const SiteMetaDataGenerator: RootMetadataGeneratorPlugin< SiteMetadata > = {
 	parser: parseSiteMetadata,
 };
 
-const defaultMetaDataGenerators = [
-	IconsMetaDataGenerator,
-	SiteMetaDataGenerator,
-];
+const DEFAULT_FETCHED_OBJECT = {};
+const DEFAULT_VALIDATED_OBJECT = {};
+const DEFAULT_PARSED_OBJECT = {};
 
 registerMetadataGenerator( IconsMetaDataGenerator, 'icons' );
 registerMetadataGenerator( SiteMetaDataGenerator, 'site' );
-
-const defaultFetchedDataObject = {};
-const defaultParsedDataObject = {};
 
 /**
  * Fetches and parses Metadata from WordPress server
@@ -68,32 +71,87 @@ const defaultParsedDataObject = {};
 export async function getRootMetadata(): Promise< Metadata > {
 	const keys = Object.keys( plugins );
 
-	// @todo Better error handelling for fetching
 	// Fetch data in parallel
-	const fetchedData = await Promise.all(
-		keys.map( async ( key ) => {
+	const fetchedDataMap: Record< string, unknown > = Object.fromEntries(
+		await Promise.all(
+			keys.map( async ( key ) => {
+				const plugin = plugins[ key ];
+				let data;
+				// Plugin not found
+				if ( ! plugin ) {
+					data = DEFAULT_FETCHED_OBJECT;
+					return [ key, data ];
+				}
+				// Plugin found try fetching
+				try {
+					data = await plugin.fetcher();
+					return [ key, data ];
+				} catch {
+					// Fetching failed used defaultFetchedObject
+					if ( plugin.defaultFetchedObject ) {
+						data = plugin.defaultFetchedObject;
+					} else {
+						data = DEFAULT_FETCHED_OBJECT;
+					}
+					// @todo add proper logging
+					return [ key, data ];
+				}
+			} )
+		)
+	);
+	const validatedDataMap = Object.fromEntries(
+		keys.map( ( key ) => {
 			const plugin = plugins[ key ];
+			let data;
+			// Plugin not found
 			if ( ! plugin ) {
-				return defaultFetchedDataObject;
+				data = DEFAULT_VALIDATED_OBJECT;
+				return [ key, data ];
 			}
-			return await plugin.fetcher();
+			// Plugin found try validating
+			try {
+				data = plugin.validator( fetchedDataMap[ key ] );
+				return [ key, data ];
+			} catch {
+				// Validation failed used defaultValidatedObject
+				if ( plugin.defaultValidatedObject ) {
+					data = plugin.defaultValidatedObject;
+				} else {
+					data = DEFAULT_VALIDATED_OBJECT;
+				}
+				// @todo add proper logging
+				return [ key, data ];
+			}
+		} )
+	);
+	const parsedDataMap = Object.fromEntries(
+		keys.map( ( key ) => {
+			const plugin = plugins[ key ];
+			let data;
+			// Plugin not found
+			if ( ! plugin ) {
+				data = DEFAULT_PARSED_OBJECT;
+				return [ key, data ];
+			}
+			// Plugin found try parsing
+			try {
+				data = plugin.parser( validatedDataMap[ key ] );
+				return [ key, data ];
+			} catch {
+				// Parsing failed used defaultParsedObject
+				if ( plugin.defaultParsedObject ) {
+					data = plugin.defaultParsedObject;
+				} else {
+					data = DEFAULT_VALIDATED_OBJECT;
+				}
+				// @todo add proper logging
+				return [ key, data ];
+			}
 		} )
 	);
 
-	// @todo Better error handelling for validation
-	const validatedData = defaultMetaDataGenerators.map(
-		( { validator }, ind ) => validator( fetchedData[ ind ] )
+	return Object.values( parsedDataMap ).reduce(
+		( acc, obj ) => Object.assign( acc, obj ),
+		{}
 	);
-
-	// Parse data. if parser not found or the parser throws use default object
-	// @todo Better error handelling for validation
-	const parsedData = defaultMetaDataGenerators.map( ( { parser }, ind ) => {
-		if ( ! validatedData[ ind ] ) {
-			return defaultParsedDataObject;
-		}
-		//@ts-ignore
-		return parser( validatedData[ ind ] );
-	} );
-
-	return parsedData.reduce( ( acc, obj ) => Object.assign( acc, obj ), {} );
 }
