71702bff9b0d74b4e513c137dede9f77afc24a4d
Swanand01 <swanandmathekar@gmail.com>
2025-04-01 12:49:31 +0530

    refactor: update build file

--- packages/cli/build.cjs
+++ packages/cli/build.cjs
@@ -1,23 +1,36 @@
 const path = require( 'path' );
 const fs = require( 'fs/promises' );
+const { exec } = require( 'child_process' );
 
 ( async () => {
-	try {
-		await fs.access( './dist' );
-		await fs.rm( './dist', { recursive: true, force: true } );
-	} catch ( error ) {}
-	await fs.mkdir( './dist', { recursive: true } );
 	const examplesDir = path.resolve( __dirname, '../../examples' );
-	const srcDir = path.resolve( __dirname, './src' );
 
-	await fs.cp( examplesDir, path.resolve( __dirname, './dist/examples' ), {
-		recursive: true,
-		filter: ( source ) =>
-			! /.*(node_modules|package-lock\.json|\.env|\.next|next-env\.d\.ts|src\/__generated)/g.test(
-				source
-			),
-	} );
-	await fs.cp( srcDir, path.resolve( __dirname, './dist' ), {
-		recursive: true,
+	console.log( 'Compiling TypeScript...' );
+	const child = exec( 'tsc --build --force' );
+	child.stdout?.pipe( process.stdout );
+	child.stderr?.pipe( process.stderr );
+
+	child.on( 'exit', async ( code ) => {
+		if ( code !== 0 ) {
+			console.error(
+				`TypeScript compilation failed with exit code ${ code }`
+			);
+			process.exit( code || 1 );
+		}
+
+		console.log( 'Copying examples...' );
+		await fs.cp(
+			examplesDir,
+			path.resolve( __dirname, './dist/examples' ),
+			{
+				recursive: true,
+				filter: ( source ) =>
+					! /.*(node_modules|package-lock\.json|\.env|\.next|next-env\.d\.ts|src\/__generated)/g.test(
+						source
+					),
+			}
+		);
+
+		console.log( 'Build complete!' );
 	} );
 } )();
