6de5af9fe96b5453df0767182629939866601091
Shah Shalin <shalinshh723@gmail.com>
2025-04-02 23:09:36 +0530

    Feature: add QueryProviders, update the flow of setting configs, update how clientOptions generics are infered

---
+++ packages/apollo/src/query-provider/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import {
+	type ApolloClient,
+	ApolloProvider,
+	type NormalizedCacheObject,
+} from '@apollo/client';
+import { type JSX, type PropsWithChildren } from 'react';
+
+/**
+ *
+ * @param root0 - The root object.
+ * @param root0.children - The child components.
+ * @param root0.client - The Apollo Client instance.
+ *
+ * @return The rendered ApolloProvider component.
+ */
+export const ApolloQueryProvider: ( {
+	client,
+	children,
+}: React.PropsWithChildren< {
+	client: ApolloClient< NormalizedCacheObject >;
+} > ) => JSX.Element = ( {
+	client,
+	children,
+}: PropsWithChildren< {
+	client: ApolloClient< NormalizedCacheObject >;
+} > ) => {
+	return <ApolloProvider client={ client }>{ children }</ApolloProvider>;
+};
