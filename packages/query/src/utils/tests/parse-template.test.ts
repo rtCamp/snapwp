5a13a33abef747d74a1a6ed714c7b6c0208be6d0
Swanand01 <swanandmathekar@gmail.com>
2025-04-09 18:54:32 +0530

    chore: add is404 test

--- packages/query/src/utils/tests/parse-template.test.ts
+++ packages/query/src/utils/tests/parse-template.test.ts
@@ -83,6 +83,7 @@ describe( 'parseQueryResult', () => {
 				{ src: 'https://cdn.com/script.js', handle: 'cdn-script' },
 			],
 			bodyClasses: [ 'class1', 'class2' ],
+			is404: false,
 		} );
 	} );
 
@@ -103,5 +104,75 @@ describe( 'parseQueryResult', () => {
 			'(Please refer to our FAQs for steps to debug and fix)',
 			{ message: 'Critical error occurred' }
 		);
+	} );
+
+	it( 'should return is404 true if `templateByUri.connectedNode` is null', () => {
+		const queryData: ApolloQueryResult< GetCurrentTemplateQuery > = {
+			data: {
+				templateByUri: {
+					bodyClasses: [ 'class1', 'class2' ],
+					enqueuedScripts: {
+						nodes: [
+							{
+								id: '122',
+								src: '/script.js',
+								handle: 'test-script',
+							},
+							{
+								id: '123',
+								src: 'https://cdn.com/script.js',
+								handle: 'cdn-script',
+							},
+						],
+					},
+					enqueuedStylesheets: {
+						nodes: [
+							{
+								src: '/style.css',
+								handle: 'test-style',
+								before: [ 'before-content' ],
+								after: [ 'after-content' ],
+							},
+						],
+					},
+					editorBlocks: [
+						{
+							type: 'core/paragraph',
+							renderedHtml: '<p>Text</p>',
+						},
+					],
+					connectedNode: null,
+				},
+			},
+			errors: [],
+			loading: false,
+			networkStatus: 7,
+		};
+
+		const result = parseQueryResult( queryData, wordpressUrl, uri );
+
+		expect( result ).toEqual( {
+			stylesheets: [
+				{
+					src: 'https://test.com/style.css',
+					handle: 'test-style',
+					before: 'before-content',
+					after: 'after-content',
+				},
+			],
+			editorBlocks: [
+				{
+					type: 'core/paragraph',
+					renderedHtml: '<p>Text</p>',
+				},
+			],
+			scriptModules: undefined,
+			scripts: [
+				{ src: 'https://test.com/script.js', handle: 'test-script' },
+				{ src: 'https://cdn.com/script.js', handle: 'cdn-script' },
+			],
+			bodyClasses: [ 'class1', 'class2' ],
+			is404: true,
+		} );
 	} );
 } );
