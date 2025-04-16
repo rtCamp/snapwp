9240dfe4d6637a03f390108fab14b2122a0607a7
Swanand01 <swanandmathekar@gmail.com>
2025-04-17 00:52:44 +0530

    revert: not-found.tsx

--- examples/nextjs/starter/src/app/[[...path]]/page.tsx
+++ examples/nextjs/starter/src/app/[[...path]]/page.tsx
@@ -1,14 +1,10 @@
 import { TemplateRenderer } from '@snapwp/next';
 import { EditorBlocksRenderer } from '@snapwp/blocks';
-import { notFound } from 'next/navigation';
 
 export default function Page() {
 	return (
 		<TemplateRenderer>
 			{ ( { editorBlocks, is404 } ) => {
-				if ( is404 ) {
-					notFound();
-				}
 				return <EditorBlocksRenderer editorBlocks={ editorBlocks } />;
 			} }
 		</TemplateRenderer>
