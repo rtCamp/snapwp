72c7affff86502eb30bd9a5a0e53727149fd7745
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2025-04-22 08:43:23 +0530

    chore: remove unused export
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -1,6 +1,5 @@
 export * from './__generated/graphql';
 export * from './query-engine';
 export * from './utils/query-generation';
-export type { TypedDocumentNode } from '@graphql-typed-document-node/core';
 export { parseQueryResult } from './utils/parse-template';
 export { print } from 'graphql';
