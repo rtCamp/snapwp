87f8f5600039e7f72f0146f3dc1f0c77ead946bd
Shah Shalin <56588503+SH4LIN@users.noreply.github.com>
2025-04-25 18:38:57 +0530

    feat: decouple query engine (#144)
    * feature: add initial apollo and tanstack client
    
    * feature: decouple apollo client  from query package, add tanstack client package
    
    * Feature: add client from snapwp.config.ts
    
    * Feature: add QueryProviders, update the flow of setting configs, update how clientOptions generics are infered
    
    * fix: eslint issues, remove query-engine test case as it will need complete re-work
    
    * fix: typecheck issue in examples/nextjs/starter
    
    * feature: add hydration boundary
    
    * fix: remove hydration boundary for now
    
    * refactor: renamed QueryEngine to QueryRepository
    
    * refactor: change queryEngine config property to required
    
    * fix: query package test cases
    
    * refactor: convert tanstack and apollo adapter class to singleton
    
    * refactor: updated comments
    
    * refactor: updated DocBlock comment with params type
    
    * feat : add import/no-default-export eslint rule.
    
    * chore : updated files for no-default-exports.
    
    * chore : add changeset.
    
    * chore : reorder-import
    
    * temp : fix failing gh actions
    
    * refactor: revert back renaming of query-engine
    
    * fix : export default for codegen config and baseConfig
    
    * refactor: reorganize adapter files for apollo and tanstack, update comments in the base interface
    
    * refactor: update package-lock.json
    
    * refactor: remove adapter semantics and replace it with engine, remove query-adapter registry
    
    * refactor: destructuring queryEngine from config, remove React import
    
    * refactor: add common import type statement instead of type to each property
    
    * refactor: update the code to allow direct import of the query client functions
    
    * refactor: rename @snapwp/apollo to @snapwp/plugin-apollo-client and @snapwp/tanstack to @snapwp/plugin-tanstack-query
    
    * refactor: update package-lock.json
    
    * refactor: replace key param with name param in fetch query
    
    * refactor: use fetchQueryArs and useQueryArgs in plugins
    
    * refactor: update package-lock.json
    
    * refactor: cleanup code
    
    * refactor: replaced name property type from string[] to string
    
    * refactor: updated script names
    
    * docs: add README.md for apollo-client and tanstack-query
    
    * fix: TS error for the QueryEngineBase class type
    
    * fix: update config manager generic type
    
    * refactor: code cleanup and fix type issue
    
    * refactor: replace tanstack dep with apollo in examples nextjs starter template
    
    * refactor: apollo query engine code cleanup
    
    * docs: add how-to-overload-query-engine document
    
    * refactor: move dependencies from dependency list to peer-dependency list, remove how to overload query engine doc
    
    * fix: test cases
    
    * fix: dependency management in scaffolded app
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    
    * chore: add changeset
    
    * chore: update changeset message
    
    * chore: cleanup
    
    ---------
    
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
    Co-authored-by: Ta5r <srv.tanay@gmail.com>
    Co-authored-by: Tanay Srivastava <62954323+Ta5r@users.noreply.github.com>
    Co-authored-by: Dovid Levine <david@axepress.dev>
    Co-authored-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

---
+++ packages/plugin-apollo-client/src/engine/index.ts
@@ -0,0 +1,187 @@
+import {
+	ApolloClient,
+	ApolloError,
+	InMemoryCache,
+	useApolloClient,
+	useQuery as useApolloQuery,
+	type ApolloClientOptions,
+	type NormalizedCacheObject,
+	type OperationVariables,
+	type QueryOptions,
+	type ServerError,
+	type ServerParseError,
+} from '@apollo/client';
+import { Logger } from '@snapwp/core';
+import { getGraphqlUrl } from '@snapwp/core/config';
+import { QueryProvider } from './query-provider';
+import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import type { QueryEngine } from '@snapwp/types';
+
+export type clientType = ApolloClient< NormalizedCacheObject >;
+export type clientOptionsType = ApolloClientOptions< NormalizedCacheObject >;
+
+type ApolloQueryArgs< TData, TQueryVars extends Record< string, unknown > > = {
+	name: string;
+	query: TypedDocumentNode< TData, TQueryVars >;
+	options?: QueryOptions< TQueryVars, TData >;
+};
+
+/**
+ * An adapter for Apollo Client that implements the QueryEngine interface.
+ * This adapter provides methods for obtaining an Apollo Client instance, executing queries,
+ * and using queries as hooks.
+ */
+export class ApolloClientEngine implements QueryEngine< clientType > {
+	private readonly client: ApolloClient< NormalizedCacheObject >;
+
+	/**
+	 * Creates a new instance of ApolloClientEngine.
+	 * @param { ApolloClientOptions< NormalizedCacheObject > } options Optional ApolloClientOptions to configure the client instance.
+	 */
+	constructor( options?: ApolloClientOptions< NormalizedCacheObject > ) {
+		const defaultOptions: ApolloClientOptions< NormalizedCacheObject > = {
+			cache: new InMemoryCache(),
+			uri: getGraphqlUrl(),
+		};
+		const mergedOptions = {
+			...defaultOptions,
+			...( ( options as Partial<
+				ApolloClientOptions< NormalizedCacheObject >
+			> ) || {} ),
+		};
+
+		this.client = new ApolloClient( mergedOptions );
+	}
+
+	/**
+	 * Returns ApolloClient instance.
+	 *
+	 * @return A instance of ApolloClient.
+	 */
+	getClient(): ApolloClient< NormalizedCacheObject > {
+		return this.client;
+	}
+
+	/**
+	 * Returns an ApolloClient instance from a provided client or undefined.
+	 * This method is useful when you need to use the ApolloClient hook.
+	 * @param { ApolloClient< NormalizedCacheObject > } client An optional ApolloClient instance.
+	 * @return The ApolloClient instance if provided; otherwise, undefined.
+	 */
+	useClient(
+		client: ApolloClient< NormalizedCacheObject > | undefined
+	): ApolloClient< NormalizedCacheObject > {
+		// eslint-disable-next-line react-hooks/rules-of-hooks -- This is a hook, so we need to use it in a React component.
+		return useApolloClient(
+			client || this.client
+		) as ApolloClient< NormalizedCacheObject >;
+	}
+
+	/**
+	 * Executes a GraphQL query using the Apollo Client instance and writes the result to the cache.
+	 * @param { Object } props An object containing:
+	 * @param { string[] } props.key - An array of strings that uniquely identifies the query in the cache.
+	 * @param { DocumentNode | TypedDocumentNode< TData > } props.query - A GraphQL DocumentNode or TypedDocumentNode representing the query.
+	 * @param { TQueryOptions } props.options - Optional query options compatible with Apollo's QueryOptions.
+	 * @return A promise that resolves with the query data of type TData.
+	 * @throws An error if the query fails, with enhanced error logging for ApolloErrors.
+	 */
+	async fetchQuery<
+		TData,
+		TQueryOptions extends Record< string, unknown >,
+	>( {
+		name,
+		query,
+		options,
+	}: ApolloQueryArgs< TData, TQueryOptions > ): Promise< TData > {
+		try {
+			const queryResult = await this.getClient().query< TData >( {
+				...options,
+				query,
+			} );
+
+			if ( queryResult.errors?.length ) {
+				queryResult.errors?.forEach( ( error ) => {
+					Logger.error( `Error fetching ${ name }: ${ error }` );
+				} );
+			}
+
+			return queryResult.data;
+		} catch ( error ) {
+			if ( error instanceof ApolloError ) {
+				logApolloErrors( error );
+
+				if ( error.networkError ) {
+					throw new Error(
+						getNetworkErrorMessage( error.networkError )
+					);
+				}
+			}
+			throw error;
+		}
+	}
+
+	/**
+	 * Executes a GraphQL query as a React hook using Apollo's useQuery.
+	 * @param { Object } props An object containing:
+	 * @param { string[] } props.key - An array of strings that uniquely identifies the query in the cache.
+	 * @param { DocumentNode | TypedDocumentNode< TData > } props.query - A GraphQL DocumentNode or TypedDocumentNode representing the query.
+	 * @param { TQueryOptions } props.options - Optional query options compatible with Apollo's QueryHookOptions.
+	 * @return The query result data of type TData.
+	 */
+	useQuery< TData, TQueryOptions extends Record< string, unknown > >( {
+		query,
+		options,
+	}: ApolloQueryArgs< TData, TQueryOptions > ): TData {
+		// eslint-disable-next-line react-hooks/rules-of-hooks -- This is a hook, so we need to use it in a React component.
+		return useApolloQuery< TData, OperationVariables >( query, options )
+			.data as TData;
+	}
+	QueryProvider = QueryProvider;
+}
+
+/**
+ * Logs Apollo errors by outputting error messages for GraphQL, client, and protocol errors.
+ * @param {ApolloError} error The ApolloError object containing error details.
+ */
+const logApolloErrors = ( error: ApolloError ): void => {
+	error.graphQLErrors.forEach( ( graphQLError ) => {
+		Logger.error( graphQLError.message );
+	} );
+	error.clientErrors.forEach( ( clientError ) => {
+		Logger.error( clientError.message );
+	} );
+	error.protocolErrors.forEach( ( protocolError ) => {
+		Logger.error( protocolError.message );
+	} );
+};
+
+/**
+ * Constructs a user-friendly network error message based on the type of network error.
+ * @param {Error | ServerParseError | ServerError} networkError The network error, which may be a generic Error, ServerParseError, or ServerError.
+ * @return A formatted string with the error message and status code.
+ */
+const getNetworkErrorMessage = (
+	networkError: Error | ServerParseError | ServerError
+): string => {
+	let statusCode: number | undefined;
+	let errorMessage: string | undefined;
+
+	if ( networkError.name === 'ServerError' ) {
+		const serverError = networkError as ServerError;
+		statusCode = serverError.statusCode;
+		if ( typeof serverError.result === 'string' ) {
+			errorMessage = serverError.result;
+		} else {
+			errorMessage = serverError.result[ 'message' ];
+		}
+	} else if ( networkError.name === 'ServerParseError' ) {
+		const serverParseError = networkError as ServerParseError;
+		statusCode = serverParseError.statusCode;
+		errorMessage = serverParseError.message;
+	} else {
+		errorMessage = networkError.message;
+	}
+
+	return `Network error ${ errorMessage } (Status: ${ statusCode })`;
+};
---
+++ packages/plugin-apollo-client/src/engine/query-provider.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import {
+	ApolloProvider,
+	type ApolloClient,
+	type NormalizedCacheObject,
+} from '@apollo/client';
+import type { JSX, PropsWithChildren } from 'react';
+
+/**
+ * Apollo Query Provider component.
+ *
+ * @param { Object } props - The root object.
+ * @param { ReactNode | undefined } props.children - The child components.
+ * @param { ApolloClient< NormalizedCacheObject > } props.client - The Apollo Client instance.
+ *
+ * @return The rendered ApolloProvider component.
+ */
+export const QueryProvider: ( {
+	client,
+	children,
+}: PropsWithChildren< {
+	client: ApolloClient< NormalizedCacheObject >;
+} > ) => JSX.Element = ( {
+	client,
+	children,
+}: PropsWithChildren< {
+	client: ApolloClient< NormalizedCacheObject >;
+} > ) => {
+	return <ApolloProvider client={ client }>{ children }</ApolloProvider>;
+};
