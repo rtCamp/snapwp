37d8b5206516bc781cf635e88b336e4cdc3f06a7
Swanand01 <swanandmathekar@gmail.com>
2025-05-05 20:12:32 +0530

    feat: add support for both public and private env vars

--- packages/types/src/global/env.ts
+++ packages/types/src/global/env.ts
@@ -4,6 +4,13 @@ declare global {
 			EDITOR?: string;
 			GRAPHQL_SCHEMA_FILE?: string;
 			INTROSPECTION_TOKEN?: string;
+			NEXT_PUBLIC_CORS_PROXY_PREFIX?: string;
+			NEXT_PUBLIC_FRONTEND_URL?: string;
+			NEXT_PUBLIC_GRAPHQL_ENDPOINT?: string;
+			NEXT_PUBLIC_REST_URL_PREFIX?: string;
+			NEXT_PUBLIC_WP_HOME_URL?: string;
+			NEXT_PUBLIC_WP_SITE_URL?: string;
+			NEXT_PUBLIC_WP_UPLOADS_DIRECTORY?: string;
 			CORS_PROXY_PREFIX?: string;
 			FRONTEND_URL?: string;
 			GRAPHQL_ENDPOINT?: string;
