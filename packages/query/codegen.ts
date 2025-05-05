510e94770dff9ce23aba27e3c8224de71f49195d
Swanand01 <swanandmathekar@gmail.com>
2025-05-05 20:12:40 +0530

    refactor: codegen.ts

--- packages/query/codegen.ts
+++ packages/query/codegen.ts
@@ -6,12 +6,18 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 dotenv.config( { path: '../../.env' } );
 
+// If there's no explicit SITE_URL, it's the same as the HOME_URL.
+const homeUrl =
+	process.env.WP_SITE_URL ||
+	process.env.NEXT_PUBLIC_WP_SITE_URL ||
+	process.env.WP_HOME_URL ||
+	process.env.NEXT_PUBLIC_WP_HOME_URL;
+
+const graphqlEndpoint =
+	process.env.GRAPHQL_ENDPOINT || process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT;
+
 // This is necessary because we don't have access to the config manager.
-const graphqlUrl = generateGraphqlUrl(
-	// If there's no explicit SITE_URL, it's the same as the HOME_URL.
-	process.env.WP_SITE_URL || process.env.WP_HOME_URL,
-	process.env.GRAPHQL_ENDPOINT
-);
+const graphqlUrl = generateGraphqlUrl( homeUrl, graphqlEndpoint );
 
 const config: CodegenConfig = {
 	...baseConfigs,
